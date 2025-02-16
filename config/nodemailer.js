104fe13229b40d1f387284963d236492db4f2d02
Bonu2005 <booonu@icloud.com>
2025-02-16 16:01:40 +0500

    compzona

--- config/nodemailer.js
+++
@@ -1,9 +0,0 @@
-import nodemailer from "nodemailer"
-
-export const transport = nodemailer.createTransport({
-    service: "icloud",
-    auth: {
-       user: "booonu@icloud.com",
-       pass: "ghbw-kknw-hbqr-srzv",
-    },
- })
